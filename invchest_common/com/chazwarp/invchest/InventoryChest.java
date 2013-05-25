2cce9b5ea3fba18775680e84856546d73a3832a0
Chazwarp923 <chazwarp923@icloud.com>
2013-05-25 15:27:30 -0400

    Initial Commit

---
+++ invchest_common/com/chazwarp/invchest/InventoryChest.java
@@ -0,0 +1,30 @@
+package com.chazwarp.invchest;
+
+import com.chazwarp.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class InventoryChest {
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ invchest_common/com/chazwarp/invchest/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.chazwarp.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "InvChest";
+    public static final String MOD_NAME = "Inventory Chest";
+    public static final String VERSION = "0.01";
+}
