ba76f6bd1448b45249bcd8d047a982839aa91f94
Chaz Kerby <chazwarp923@icloud.com>
2013-07-11 23:54:36 -0400

    Added a packet handler

--- invchest_common/com/chazwarp/invchest/InventoryChest.java
+++ invchest_common/com/chazwarp/invchest/InventoryChest.java
@@ -1,17 +1,20 @@
 package com.chazwarp.invchest;
 
+import com.chazwarp.invchest.network.PacketHandler;
 import com.chazwarp.lib.Reference;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
-//@NetworkMod(channels = { Reference.CHANNEL_NAME }, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
+@NetworkMod(channels = { Reference.CHANNEL_NAME }, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class InventoryChest {
 
     @Instance(Reference.MOD_ID)
--- invchest_common/com/chazwarp/invchest/network/PacketHandler.java
+++ invchest_common/com/chazwarp/invchest/network/PacketHandler.java
@@ -1,12 +1,15 @@
 package com.chazwarp.invchest.network;
 
-public class PacketHandler {
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet250CustomPayload;
+import cpw.mods.fml.common.network.IPacketHandler;
+import cpw.mods.fml.common.network.Player;
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+public class PacketHandler implements IPacketHandler{
+
+	@Override
+	public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player) {
+		// TODO Auto-generated method stub
 
     }
 
