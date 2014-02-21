46af5b33ae08e4eb8955ae6c1c475be7ad1e5368
Chazwarp923 <chazwarp923@icloud.com>
2014-02-21 15:39:24 -0500

    t

--- src/com/chazwarp/invchest/client/ClientProxy.java
+++ src/com/chazwarp/invchest/client/ClientProxy.java
@@ -4,12 +4,10 @@
 package com.chazwarp.invchest.client;
 
 import com.chazwarp.invchest.CommonProxy;
-import com.chazwarp.invchest.lib.BlockInfo;
 import com.chazwarp.invchest.render.PresentRenderer;
 import com.chazwarp.invchest.tileentity.TileEntityPresent;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy {
        
